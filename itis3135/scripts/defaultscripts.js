b4f786200cf1ef1a5bdcef2d5dfd400720b332a4
ckells01 <ckells@uncc.edu>
2022-02-10 18:47:57 -0500

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -2,3 +2,10 @@ function scriptTest()
 {
     alert("My script is running!");
 }
+
+function welcome()
+{
+    let name = document.getElementById("name").value;
+    let feeling = document.getElementById("feeling").value;
+    alert("The Charcoal Komodo Dragon welcomes you, Were glad you are doing <span></span>!");
+}
