55549eba23739a2477badf0fb11ec54cded10669
ckells01 <ckells@uncc.edu>
2022-02-11 18:02:04 -0500

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -43,9 +43,14 @@ function aveageDragonAge()
 
     averageAge = parseInt(total/3);
 
-    document.write("<h1>The Komodo Dragon Age App</h1>"); 
+    alert("Age 1 = " + age1 + "<br>" + 
+        "Age 2 = " + age2 + "<br>" + 
+        "Age 3 = " + age3 + "<br><br>" + 
+        "Average Komodo Dragon Age = " + averageAge + "<br><br>");
+
+    /** document.write("<h1>The Komodo Dragon Age App</h1>"); 
     document.write("Age 1 = " + age1 + "<br>" + 
         "Age 2 = " + age2 + "<br>" + 
         "Age 3 = " + age3 + "<br><br>" + 
-        "Average Komodo Dragon Age = " + averageAge + "<br><br>"); 
+        "Average Komodo Dragon Age = " + averageAge + "<br><br>"); **/
 }
