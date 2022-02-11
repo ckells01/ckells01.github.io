bb354e278798bf8660f59d7d2a853d645a06cc22
ckells01 <ckells@uncc.edu>
2022-02-11 17:40:49 -0500

    update

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -10,3 +10,14 @@ function welcome()
     let fullwelcome = "The Charcoal Komodo Dragons welcome you, " + name + "! We're glad you are doing " + feeling + "!";
     document.getElementById("output").innerHTML = fullwelcome;
 }
+
+function dragonsPerEnclosure()
+{
+    var numberDragons = prompt("Enter the number of Komodo Dragons"); 
+    numberDragons = parseFloat(numberDragons); 
+    var numberEnclosures = prompt("Enter the number of enclosures"); 
+    numberEnclosures = parseFloat(numberEnclosures); 
+    var dpe = numberDragons/numberEnclosures; 
+    dpe = parseInt(dpe); 
+    alert("Dragons Per Enclosure = " + dpe); 
+}
