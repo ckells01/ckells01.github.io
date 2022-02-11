057a1e35fea259f0a7b81fc6e9f6136fac71fe61
ckells01 <ckells@uncc.edu>
2022-02-10 23:50:54 -0500

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -7,6 +7,6 @@ function welcome()
 {
     let name = document.getElementById("name").value;
     let feeling = document.getElementById("feeling").value;
-    let fullwelcome = "The Charcoal Komodo Dragons welcomes you, " + name + "! We're glad you are doing " + feeling + "!";
+    let fullwelcome = "The Charcoal Komodo Dragons welcome you, " + name + "! We're glad you are doing " + feeling + "!";
     document.getElementById("output").innerHTML = fullwelcome;
 }
