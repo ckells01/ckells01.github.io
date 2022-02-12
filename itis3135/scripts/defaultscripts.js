bd86bbe38f55e18c4cb0cd78d69096e364514d31
ckells01 <ckells@uncc.edu>
2022-02-11 19:03:57 -0500

    update

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -7,8 +7,8 @@ function welcome()
 {
     let name = document.getElementById("name").value;
     let feeling = document.getElementById("feeling").value;
-    let fullwelcome = "The Charcoal Komodo Dragons welcome you, " + name + "! We're glad you are doing " + feeling + "!";
-    document.getElementById("output").innerHTML = fullwelcome;
+    let fullWelcome = "The Charcoal Komodo Dragons welcome you, " + name + "! We're glad you are doing " + feeling + "!";
+    document.getElementById("welcomeoutput").innerHTML = fullWelcome;
 }
 
 function dragonsPerEnclosure()
@@ -44,4 +44,32 @@ function aveageDragonAge()
     averageAge = parseInt(total/3);
 
     alert("Average Komodo Dragon Age = " + averageAge);
+}
+
+function timeAndDate()
+{
+    var current = new Date();
+    var monthNumber = current.getMonth() + 1;
+    var day = current.getDay();
+    var year = current.getFullYear();  
+    var date = current.getDate();
+    var hour = current.getHours();
+    var minute = current.getMinutes();
+    var dayOrNight;
+
+    var monthName = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    var dayOfTheWeek = new Array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+
+    if(hour >= 12)
+    {
+        hour %= 12;
+        dayOrNight = "pm";
+    }
+    else
+    {
+        dayOrNight = "am";
+    }
+
+    let fullTimeDate = "Today is " + hour + ":" + minute + dayOrNight + " on " + dayOfTheWeek[day] + ", " + date + " " + monthName[monthNumber] + " " + year
+    document.getElementById("timeoutput").innerHTML = fullTimeDate;
 }
