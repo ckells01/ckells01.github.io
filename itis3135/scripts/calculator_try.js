9305f475523be7bdf02bfcacb0e01715dd08d8e5
ckells01 <ckells@uncc.edu>
2022-02-23 18:51:35 -0500

    update

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,8 +1,12 @@
-/**Functions to do math*/
-function add (num1, num2)
+/**Functions to do math with alerts/prompts*/
+function add ()
 {
-    var result = num1 + num2;
-    return result;
+    var num1 = prompt("Enter the first number to add"); 
+    num1 = parseFloat(num1);
+    var num2 = prompt("Enter the second number to add"); 
+    num2 = parseFloat(num2); 
+    var result = num1 + num2; 
+    alert(num1 + " + " + num2 + " = " + result); 
 }
 
 function sub (num1, num2)
