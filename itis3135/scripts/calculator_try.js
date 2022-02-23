6d35fc3657922c438e07ff8896562d5a3f3f66b3
ckells01 <ckells@uncc.edu>
2022-02-23 18:55:07 -0500

    Update calculator_try.js

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -11,18 +11,30 @@ function add ()
 
 function sub (num1, num2)
 {
-    var result = num1 - num2;
-    return result;
+    var num1 = prompt("Enter the first number to subtract"); 
+    num1 = parseFloat(num1);
+    var num2 = prompt("Enter the second number to subtract"); 
+    num2 = parseFloat(num2); 
+    var result = num1 - num2; 
+    alert(num1 + " - " + num2 + " = " + result);
 }
 
 function mul (num1, num2)
 {
-    var result = num1 * num2;
-    return result;
+    var num1 = prompt("Enter the first number to multiply"); 
+    num1 = parseFloat(num1);
+    var num2 = prompt("Enter the second number to multiply"); 
+    num2 = parseFloat(num2); 
+    var result = num1 + num2; 
+    alert(num1 + " * " + num2 + " = " + result);
 }
 
 function div (num1, num2)
 {
-    var result = num1 / num2;
-    return result;
+    var num1 = prompt("Enter the first number to divide"); 
+    num1 = parseFloat(num1);
+    var num2 = prompt("Enter the second number to divide"); 
+    num2 = parseFloat(num2); 
+    var result = num1 + num2; 
+    alert(num1 + " / " + num2 + " = " + result);
 }
