ca3fde79c535d89d5f2fdcc0eab7e6a5666d90d5
ckells01 <ckells@uncc.edu>
2022-02-23 17:54:06 -0500

    update

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,4 +1,23 @@
-function add ()
+function add (num1, num2)
 {
-    
+    var result = num1 + num2;
+    return result;
+}
+
+function sub (num1, num2)
+{
+    var result = num1 - num2;
+    return result;
+}
+
+function mul (num1, num2)
+{
+    var result = num1 * num2;
+    return result;
+}
+
+function div (num1, num2)
+{
+    var result = num1 / num2;
+    return result;
 }
