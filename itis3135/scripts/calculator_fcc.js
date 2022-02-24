dd0771bf2fdcc8616793b0461b472ca0817dd8a6
ckells01 <ckells@uncc.edu>
2022-02-23 19:52:07 -0500

    Update calculator_fcc.js

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -1,13 +1,13 @@
 const calculate = (n1, operator, n2) => {
-    let result = ''
+    let result = ""
     
-    if (operator === 'add') {
+    if (operator === "add") {
       result = parseFloat(n1) + parseFloat(n2)
-    } else if (operator === 'subtract') {
+    } else if (operator === "subtract") {
       result = parseFloat(n1) - parseFloat(n2)
-    } else if (operator === 'multiply') {
+    } else if (operator === "multiply") {
       result = parseFloat(n1) * parseFloat(n2)
-    } else if (operator === 'divide') {
+    } else if (operator === "divide") {
       result = parseFloat(n1) / parseFloat(n2)
     }
     
@@ -15,7 +15,7 @@ const calculate = (n1, operator, n2) => {
   }
 
 const calculator = document.querySelector(".calculator")
-const display = calculator.querySelector('.calculator__display')
+const display = calculator.querySelector(".calculator__display")
 const keys = calculator.querySelector(".calculator__keys")
 
 keys.addEventListener("click", e => {
@@ -28,10 +28,10 @@ keys.addEventListener("click", e => {
         const previousKeyType = calculator.dataset.previousKeyType
 
         // Remove .is-depressed class from all keys
-        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove("is-depressed"))
 
         if (!action) {
-            if (displayedNum === '0' || previousKeyType === 'operator') {
+            if (displayedNum === "0" || previousKeyType === "operator") {
                 display.textContent = keyContent
             } else {
                 display.textContent = displayedNum + keyContent
@@ -39,24 +39,24 @@ keys.addEventListener("click", e => {
         }
 
         if (
-            action === 'add' || action === 'subtract' ||
-            action === 'multiply' || action === 'divide') {
-            key.classList.add('is-depressed')
+            action === "add" || action === "subtract" ||
+            action === "multiply" || action === "divide") {
+            key.classList.add("is-depressed")
             // Add custom attribute
-            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.previousKeyType = "operator"
             calculator.dataset.firstValue = displayedNum
             calculator.dataset.operator = action
         }
 
-        if (action === 'decimal') {
-            display.textContent = displayedNum + '.'
+        if (action === "decimal") {
+            display.textContent = displayedNum + "."
         }
 
-        if (action === 'clear') {
-            console.log('clear key!')
+        if (action === "clear") {
+            console.log("clear key!")
         }
 
-        if (action === 'calculate') {
+        if (action === "calculate") {
             const firstValue = calculator.dataset.firstValue
             const operator = calculator.dataset.operator
             const secondValue = displayedNum
