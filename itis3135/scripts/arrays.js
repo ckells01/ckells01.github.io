2cb30d0556685c86d35ef2f1d16675cb36eac480
ckells01 <ckells@uncc.edu>
2022-02-25 20:30:46 -0500

    update

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,31 +1,41 @@
-var people = [];
-var saleries = [];
+var name = [];
+var salary = [];
 
-function addSalary()
-{
+function addSalary() {
 
     var enteredSalary;
     var enteredName;
 
-    if(NaN(enteredSalary) || enteredSalary == "" ) 
-    {
+    if (NaN(enteredSalary) || enteredSalary == "") {
         alert("Please enter a valid salary");
         enteredSalary = "";
     }
-    else
-    {
+    else {
         enteredSalary = parseFloat(enteredSalary);
     }
 
 }
 
-function displayResults()
-{
+function displayResults() {
+
+    // Used to find average
+    var average = 0;
+    var sum = 0;
+    // Used to find highest salary
+    var nameWithHighestNum;
+    var maxNum;
+
+    // Loop through array of salaries to add all the salaries and 
+    // then divide by the number of saleries to find average salary
+    for (i = 0; i <= salary.length; i++) {
+        sum += salary[i];
+    }
+    average = (sum / salary.length);
+    alert("Average Salary: $" + average);
 
 }
-function displaySalary()
-{
-    
-    
+function displaySalary() {
+
+
 
 }
