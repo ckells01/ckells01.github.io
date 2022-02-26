4b30d23171f1a87d4097292d2062e701fbe32756
ckells01 <ckells@uncc.edu>
2022-02-25 20:53:45 -0500

    Update arrays.js

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,7 +1,9 @@
+// Loads functions for each button and loads ability to start user on employee selection
 window.onload = function () {
     document.getElementById("add_salary").onclick = addSalary;
     document.getElementById("display_results").onclick = displayResults;
     document.getElementById("display_salary").onclick = displaySalary;
+    document.getElementById("employee").focus();
 };
 
 var employee = [];
@@ -9,17 +11,25 @@ var salary = [];
 
 function addSalary() {   
 
+    // Get inputted employyee and their salary
     var enteredSalary = document.getElementById("employee").value;
     var enteredName = document.getElementById("salary").value;
 
+    // Check to make sure inputted salary is valid. Must be a number and must not be left empty
     if (NaN(enteredSalary) || enteredSalary == "") {
         alert("Please enter a valid salary");
-        enteredSalary = "";
+        enteredSalary = ""; // Clears invalid input
+        $("employee").focus(); // Starts user on employee selection when pages loads
     }
     else {
+        // If valid salary input, add salary and employye to arrays
         enteredSalary = parseFloat(enteredSalary);
         employee.push(enteredName);
         salary.push(enteredSalary);
+        // Clears enterybox so new salary can be entered after adding previously enter salary to array
+        // and redirects user to employee selection so a new employee and salary can be added
+        $("salary").innerHTML = "" ;
+        $("names").focus();
     }
 
 }
@@ -40,7 +50,6 @@ function displayResults() {
     }
     average = (sum / salary.length);
     alert("Average Salary: $" + average);
-
 }
 
 function displaySalary() {
