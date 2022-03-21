a28814add2823458e946481f25f56907ee06e8bd
ckells01 <ckells@uncc.edu>
2022-03-21 15:47:42 -0400

    up

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,24 @@
+"use strict";
+$(document).ready( () => {
+
+    let imageCashe = [];
+    $("#slides img").each( (index, img) => {
+        const image = new Image();
+        image.src = $(img).attr("src"); 
+        image.title = $(img).attr("alt");
+        imageCashe[index] = image;
+    });
+
+    let imageCounter = 0;
+    setInterval( () => {
+        $("#caption").fadeOut(1000);
+        $("#slide").fadeOut(1000);
+            () => {
+                imageCounter = (imageCounter + 1) % imageCashe.length;
+                const nextImage = imageCashe[imageCounter];
+                $("#slide").attr("src", nextImage.src).fadeIn(1000);
+                $("#caption").text(nextImage.title).fadeIn(1000);
+            };
+    }, 
+    3000);
+});
