e570666e1af2211e6d7557b9bc2abcd14217ab41
ckells01 <ckells@uncc.edu>
2022-03-23 17:23:30 -0400

    up

--- itis3135/hobby/hobby.js
+++ itis3135/hobby/hobby.js
@@ -1,23 +1,20 @@
-function openTab(evt, tabNum) {
+function openTab(event, tabNum) {
 
-    // Declare all variables
-    var i, tabcontent, tablinks;
+    var i;
+    var tabcontent;
 
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
     tabButton = document.getElementsByClassName("tabButton");
     for (i = 0; i < tabButton.length; i++) {
         tabButton[i].className = tabButton[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabNum).style.display = "block";
-    evt.currentTarget.className += " active";
+    event.currentTarget.className += " active";
 }
 
 document.getElementById("defaultOpen").click();
