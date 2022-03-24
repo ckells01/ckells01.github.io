0ec34c53d0f864d1e4e25a282bed8368c3d8dba9
ckells01 <ckells@uncc.edu>
2022-03-24 14:03:20 -0400

    up

--- itis3135/hobby/hobby.js
+++ itis3135/hobby/hobby.js
@@ -1,3 +1,7 @@
+window.onload = function () {
+    document.getElementById("genre_response").onclick = genreResponse;
+};
+
 function openTab(event, tabNum) {
 
     var i;
@@ -18,3 +22,15 @@ function openTab(event, tabNum) {
 }
 
 document.getElementById("defaultOpen").click();
+
+function genreResponse() {
+
+    var response = document.getElementById("genre_response").value;
+    if (response == "yes") {
+        document.getElementById("genre_msg").innerHTML = "Thats awesome! I hope you enjoyed!";
+    }
+    if (response == "no") {
+        document.getElementById("genre_msg").innerHTML = "You should go listen! I hope you enjoy!";
+    }
+
+}
