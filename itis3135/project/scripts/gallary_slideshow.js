87c4d893cb05faf9c9f30fa4197c8a804e578973
ckells01 <ckells@uncc.edu>
2022-03-26 18:13:23 -0400

    up

--- itis3135/project/scripts/gallary_slideshow.js
+++ itis3135/project/scripts/gallary_slideshow.js
@@ -1,6 +1,32 @@
 window.onload = function () {
 
-let slideIndex = 1;
+    "use strict";
+$(document).ready(() => {
+
+    let nextSlide = $("#slides img:first-child");
+
+    setInterval( () => {
+        $("#caption").fadeOut(1000);
+        $("#slide").fadeOut(1000,
+            () => {
+                if (nextSlide.next().length == 0) {
+                    nextSlide = $("#slides img:first-child");
+                } else {
+                    nextSlide = nextSlide.next();
+                }
+                const nextSlideSource = nextSlide.attr("src");
+                const nextCaption = nextSlide.attr("alt");
+                $("#slide").attr("src", nextSlideSource).fadeIn(1000);
+                $("#caption").text(nextCaption).fadeIn(1000);
+            });
+    }, 3000);
+});
+}
+
+
+
+
+/**let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -30,4 +56,4 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
-}
+*/
