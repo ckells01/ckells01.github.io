5f58c11bf3623d3e3a75d60306d7fe6de4f52936
ckells01 <ckells@uncc.edu>
2022-03-26 18:18:21 -0400

    Update gallary_slideshow.js

--- itis3135/project/scripts/gallary_slideshow.js
+++ itis3135/project/scripts/gallary_slideshow.js
@@ -1,26 +1,26 @@
 window.onload = function () {
 
     "use strict";
-$(document).ready(() => {
+    $(document).ready(() => {
 
-    let nextSlide = $("#slides img:first-child");
+        let nextSlide = $("#slides img:first-child");
 
-    setInterval( () => {
-        $("#caption").fadeOut(1000);
-        $("#slide").fadeOut(1000,
-            () => {
-                if (nextSlide.next().length == 0) {
-                    nextSlide = $("#slides img:first-child");
-                } else {
-                    nextSlide = nextSlide.next();
-                }
-                const nextSlideSource = nextSlide.attr("src");
-                const nextCaption = nextSlide.attr("alt");
-                $("#slide").attr("src", nextSlideSource).fadeIn(1000);
-                $("#caption").text(nextCaption).fadeIn(1000);
-            });
-    }, 3000);
-});
+        setInterval(() => {
+            $("#caption").fadeOut(1000);
+            $("#slide").fadeOut(1000,
+                () => {
+                    if (nextSlide.next().length == 0) {
+                        nextSlide = $("#slides img:first-child");
+                    } else {
+                        nextSlide = nextSlide.next();
+                    }
+                    const nextSlideSource = nextSlide.attr("src");
+                    const nextCaption = nextSlide.attr("alt");
+                    $("#slide").attr("src", nextSlideSource).fadeIn(1000);
+                    $("#caption").text(nextCaption).fadeIn(1000);
+                });
+        }, 3000);
+    });
 }
 
 
