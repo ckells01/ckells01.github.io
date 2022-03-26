ea7b72b8c6359277eca9ea7c98a04a2b6cdab4a1
ckells01 <ckells@uncc.edu>
2022-03-26 17:19:33 -0400

    up

--- itis3135/project/scripts/gallary_slideshow.js
+++ itis3135/project/scripts/gallary_slideshow.js
@@ -1,23 +1,30 @@
-window.onload = function () {
-"use strict";
-$(document).ready(() => {
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    let nextSlide = $("#slides img:first-child");
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    setInterval( () => {
-        $("#caption").fadeOut(1000);
-        $("#slide").fadeOut(1000,
-            () => {
-                if (nextSlide.next().length == 0) {
-                    nextSlide = $("#slides img:first-child");
-                } else {
-                    nextSlide = nextSlide.next();
-                }
-                const nextSlideSource = nextSlide.attr("src");
-                const nextCaption = nextSlide.attr("alt");
-                $("#slide").attr("src", nextSlideSource).fadeIn(1000);
-                $("#caption").text(nextCaption).fadeIn(1000);
-            });
-    }, 3000);
-});
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
