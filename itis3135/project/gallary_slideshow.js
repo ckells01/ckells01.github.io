6516f865dc7bddb17a99d9d31ac9ad07ce4b6c4e
ckells01 <ckells@uncc.edu>
2022-03-26 16:56:15 -0400

    up

--- itis3135/project/gallary_slideshow.js
+++ itis3135/project/gallary_slideshow.js
@@ -1,1 +1,22 @@
-//Put working Activity 9 Slideshow code here after finishing it, using oics for metal archive.
+"use strict";
+$(document).ready(() => {
+
+    let nextSlide = $("#slides img:first-child");
+
+    setInterval( () => {
+        $("#caption").fadeOut(1000);
+        $("#slide").fadeOut(1000,
+            () => {
+                if (nextSlide.next().length == 0) {
+                    nextSlide = $("#slides img:first-child");
+                } else {
+                    nextSlide = nextSlide.next();
+                }
+                const nextSlideSource = nextSlide.attr("src");
+                const nextCaption = nextSlide.attr("alt");
+                $("#slide").attr("src", nextSlideSource).fadeIn(1000);
+                $("#caption").text(nextCaption).fadeIn(1000);
+            });
+    }, 3000);
+});
+}
