d85703c49c3154484df59f72329f8c586d284235
ckells01 <ckells@uncc.edu>
2022-03-30 21:45:55 -0400

    up

--- itis3135/activity10/kells_activity10.js
+++ itis3135/activity10/kells_activity10.js
@@ -1,15 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
         // preload the image for each link
 
         // set up the event handlers for each link
 
-    			// get the image URL and caption for each image and animate the caption
+        // get the image URL and caption for each image and animate the caption
 
-            // cancel the default action of each link
-
-
-    // move the focus to the first link
-
+        // cancel the default action of each link
+        evt.preventDefault();
+        // move the focus to the first link
+        $("li:first-child a").focus();
 }); // end ready
