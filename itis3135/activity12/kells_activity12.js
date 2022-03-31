f4163c3cfb8a31d13e5a030a17360cfde3ce9d49
ckells01 <ckells@uncc.edu>
2022-03-31 13:57:11 -0400

    Update kells_activity12.js

--- itis3135/activity12/kells_activity12.js
+++ itis3135/activity12/kells_activity12.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $("#slider").bxSlider({
         auto: true,
-        minSlides: 1,
-        maxSlides: 1,
+        minSlides: 1, // 2 to 1
+        maxSlides: 1, // 2 to 1
         slideWidth: 300,
         slideMargin: 20,
         // changes
