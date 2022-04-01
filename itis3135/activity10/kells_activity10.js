0ddbee06b94abb0fbd3b0f89e79dc3ef96e15242
ckells01 <ckells@uncc.edu>
2022-04-01 16:24:09 -0400

    Update kells_activity10.js

--- itis3135/activity10/kells_activity10.js
+++ itis3135/activity10/kells_activity10.js
@@ -1,3 +1,4 @@
+/**
 $(document).ready(function () {
         $("#image_list a").each(function () {
 
@@ -24,4 +25,33 @@ $(document).ready(function () {
         });
         // move the focus to the first link
         $("li:first-child a").focus();
+}); // end ready
+*/
+
+$(document).ready(function () {
+    // preload images
+    $("#image_list a").each(function () {
+        var galleryImage = new Image();
+        galleryImage.src = $(this).attr("href");
+    });
+
+    // set up event handlers for links  
+    $("#image_list a").click(function (evt)
+    {
+        var imageURL = $(this).attr("href");
+        $("#image").attr("src", imageURL);
+        var caption = $(this).attr("title");
+        $("#caption").fadeOut(1000, function ()
+        {
+            $("#caption").text(caption).fadeIn(1000);
+        });
+        $("#image").fadeOut(1000, function ()
+        {
+            $("#image").attr("src", imageURL).fadeIn(1000);
+        });
+        // cancel the default action of the link
+        evt.preventDefault();
+    }); // end click
+    // move focus to first thumbnail
+    $("li:first-child a").focus();
 }); // end ready
