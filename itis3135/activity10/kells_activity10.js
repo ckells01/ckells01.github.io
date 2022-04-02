694b703b9c6813595abbc97f569be2af4e66a6f4
ckells01 <ckells@uncc.edu>
2022-04-02 19:19:59 -0400

    Update kells_activity10.js

--- itis3135/activity10/kells_activity10.js
+++ itis3135/activity10/kells_activity10.js
@@ -29,29 +29,28 @@ $(document).ready(function () {
 */
 
 $(document).ready(function () {
-    // preload images
-    $("#image_list a").each(function () {
-        var galleryImage = new Image();
-        galleryImage.src = $(this).attr("href");
-    });
 
-    // set up event handlers for links  
-    $("#image_list a").click(function (evt)
-    {
         var imageURL = $(this).attr("href");
-        $("#image").attr("src", imageURL);
         var caption = $(this).attr("title");
-        $("#caption").fadeOut(1000, function ()
-        {
-            $("#caption").text(caption).fadeIn(1000);
+
+        // preload images
+        $("#image_list a").each(function () {
+                var galleryImage = new Image();
+                galleryImage.src = imageURL
         });
-        $("#image").fadeOut(1000, function ()
-        {
-            $("#image").attr("src", imageURL).fadeIn(1000);
-        });
-        // cancel the default action of the link
-        evt.preventDefault();
-    }); // end click
-    // move focus to first thumbnail
-    $("li:first-child a").focus();
+
+        // set up event handlers for links  
+        $("#image_list a").click(function (evt) {
+                $("#image").attr("src", imageURL);
+                $("#caption").fadeOut(1000, function () {
+                        $("#caption").text(caption).fadeIn(1000);
+                });
+                $("#image").fadeOut(1000, function () {
+                        $("#image").attr("src", imageURL).fadeIn(1000);
+                });
+                // cancel the default action of the link
+                evt.preventDefault();
+        }); // end click
+        // move focus to first thumbnail
+        $("li:first-child a").focus();
 }); // end ready
