9743fd52f7c5092a2c08ecff84d68c89c9c49b45
ckells01 <ckells@uncc.edu>
2022-04-02 20:25:58 -0400

    Update kells_activity10.js

--- itis3135/scripts/kells_activity10.js
+++ itis3135/scripts/kells_activity10.js
@@ -1,3 +1,4 @@
+/**
 $(document).ready(function () {
 
         // Make vars so it is easier to use "href" and "title"
@@ -30,4 +31,35 @@ $(document).ready(function () {
         // move focus to first thumbnail
         $("li:first-child a").focus();
 
+}); // end ready
+*/
+
+$(document).ready(function ()
+{
+    // preload images
+    $("#image_list a").each(function ()
+    {
+        var swappedImage = new Image();
+        swappedImage.src = $(this).attr("href");
+    });
+
+    // set up event handlers for links  
+    $("#image_list a").click(function (evt)
+    {
+        var imageURL = $(this).attr("href");
+        $("#image").attr("src", imageURL);
+        var caption = $(this).attr("title");
+        $("#caption").fadeOut(1000, function ()
+        {
+            $("#caption").text(caption).fadeIn(1000);
+        });
+        $("#image").fadeOut(1000, function ()
+        {
+            $("#image").attr("src", imageURL).fadeIn(1000);
+        });
+        // cancel the default action of the link
+        evt.preventDefault();
+    }); // end click
+    // move focus to first thumbnail
+    $("li:first-child a").focus();
 }); // end ready
