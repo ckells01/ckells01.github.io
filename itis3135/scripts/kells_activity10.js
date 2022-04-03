e5460d398c3318c1be2d7e9312eb6e56d023676e
ckells01 <ckells@uncc.edu>
2022-04-02 21:01:44 -0400

    Update kells_activity10.js

--- itis3135/scripts/kells_activity10.js
+++ itis3135/scripts/kells_activity10.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
         // set up event handlers for links
         // swaps image and caption by fading every 1 second  
         $("#image_list a").click(function (evt) {
+                // cancel the default action of the link
+                evt.preventDefault();
                 // find out what image was clicked
                 var imageURL = $(this).attr("href");
                 $("#image").attr("src", imageURL);
@@ -22,43 +24,10 @@ $(document).ready(function () {
                 $("#image").fadeOut(1000, function () {
                         $("#image").attr("src", imageURL).fadeIn(1000);
                 });
-                // cancel the default action of the link
-                evt.preventDefault();
+
         }); // end click
 
         // move focus to first thumbnail
         $("li:first-child a").focus();
 
 }); // end ready
-
-/** 
-$(document).ready(function ()
-{
-    // preload images
-    $("#image_list a").each(function ()
-    {
-        var swappedImage = new Image();
-        swappedImage.src = $(this).attr("href");
-    });
-
-    // set up event handlers for links  
-    $("#image_list a").click(function (evt)
-    {
-        var imageURL = $(this).attr("href");
-        $("#image").attr("src", imageURL);
-        var caption = $(this).attr("title");
-        $("#caption").fadeOut(1000, function ()
-        {
-            $("#caption").text(caption).fadeIn(1000);
-        });
-        $("#image").fadeOut(1000, function ()
-        {
-            $("#image").attr("src", imageURL).fadeIn(1000);
-        });
-        // cancel the default action of the link
-        evt.preventDefault();
-    }); // end click
-    // move focus to first thumbnail
-    $("li:first-child a").focus();
-}); // end ready
-*/
