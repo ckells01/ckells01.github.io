21ee396406934bda941254f7adc95ddd4cd46063
ckells01 <ckells@uncc.edu>
2022-04-21 14:29:05 -0400

    up

--- itis3135/activity15/kells_activity15.js
+++ itis3135/activity15/kells_activity15.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
+    // Get data from JSON file. Can get it directly unlike in activity 13
+    $.getJSON("facultyList.json", function (data) { 
+        // For each faculty member in the JSON file
+        $.each(data, function () {
+            // Add to "faculty" div in html
+            $("#faculty").append(
+                "<img>" + value.image + "</img>" +
+                "<h2>" + value.full_name + "</h1>" +
+                "<h3>" + value.department + "</h1>" +
+                "<p>" + value.bio + "</h1>"
+            );
+        });
+    });
 });
