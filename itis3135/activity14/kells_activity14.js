24c903bb11b5150de171d23108ea634c9d41b690
ckells01 <ckells@uncc.edu>
2022-04-21 13:53:02 -0400

    Update kells_activity14.js

--- itis3135/activity14/kells_activity14.js
+++ itis3135/activity14/kells_activity14.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
     // list of speakers
-    $("nav_list li").click(function() {
+    $("#nav_list li").click(function () {
         let title = $(this).children("a").attr("title");
         // find json file
-        $.get(title + ".json", function (date, status) {
+        $.get(title + ".json", function (data, status) {
             // get data from json files and output on html
-            data = data[speakers][0];
-            $("main h1").html(data["title"]);
-            $("main h2").html(data["month"]);
-            $("main h3").html(data["speaker"]);
-            $("main img").html("src", data.image);
+            data = data['speakers'][0];
+            $("main h1").html(data['title']);
+            $("main h2").html(data['month']);
+            $("main h3").html(data['speaker']);
+            $("main img").attr("src", data.image);
             $("main p").html(data.text);
         });
     });
