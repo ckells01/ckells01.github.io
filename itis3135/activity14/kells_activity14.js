ab33df5dad9a3f2920583d0a489a06de3b5c6cc6
ckells01 <ckells@uncc.edu>
2022-04-21 13:47:15 -0400

    up

--- itis3135/activity14/kells_activity14.js
+++ itis3135/activity14/kells_activity14.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-
+    // list of speakers
+    $("nav_list li").click(function() {
+        let title = $(this).children("a").attr("title");
+        // find json file
+        $.get(title + ".json", function (date, status) {
+            // get data from json files and output on html
+            data = data[speakers][0];
+            $("main h1").html(data["title"]);
+            $("main h2").html(data["month"]);
+            $("main h3").html(data["speaker"]);
+            $("main img").html("src", data.image);
+            $("main p").html(data.text);
+        });
+    });
 }); // end ready
