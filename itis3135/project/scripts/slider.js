c1e5c373bbca4f39180abb7a836fb1e5df538435
ckells01 <ckells@uncc.edu>
2022-04-26 00:43:32 -0400

    Update slider.js

--- itis3135/project/scripts/slider.js
+++ itis3135/project/scripts/slider.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
     $('.bxslider').bxSlider({
-        minSlides: 1,
-        maxSlides: 1,
+        //minSlides: 1,
+        //maxSlides: 1,
         slideWidth: 300,
-        adaptiveHeight: true,
         captions: true,
-        speed: 1000,
-        auto: true
     });
 });
